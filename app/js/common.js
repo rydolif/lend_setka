75b0b13d9ce6cf3caf1eaefb0c951e6f6ad4ab5a
rudolif <rudolifrudolif@gmail.com>
2019-07-27 21:51:02 +0200

    first+last commit

---
+++ app/js/common.js
@@ -0,0 +1,82 @@
+$(function() {
+
+
+//-------------------------------попандер---------------------------------------
+  $('.modal').popup({transition: 'all 0.6s'});
+
+//------------------------------slide-----------------------------
+  var swiper = new Swiper('.apartments__slider', {
+    effect: 'fade',
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.apartments__next',
+      prevEl: '.apartments__prev',
+    },
+  });
+
+//------------------------------гамбургер-----------------------------
+  $('.hamburger').click(function() {
+    $(this).toggleClass('hamburger--active');
+    $('nav').toggleClass('nav--active');
+    $('header').toggleClass('header--menu');
+  });
+
+//------------------------------------form-------------------------------------------
+  $('input[type="tel"]').mask('+0 (000) 000-00-00');
+
+  jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+     return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+  }, "Введите Ваш телефон");
+
+  $(".form").each(function(index, el) {
+    $(el).addClass('form-' + index);
+
+    $('.form-' + index).validate({
+      rules: {
+        phone: {
+          required: true,
+          phoneno: true
+        },
+        name: 'required',
+      },
+      messages: {
+        name: "Введите Ваше имя",
+        mail: "Введите Ваш e-mail",
+      },
+      submitHandler: function(form) {
+        var t = {
+          name: jQuery('.form-' + index).find("input[name=name]").val(),
+          mail: jQuery('.form-' + index).find("input[name=mail]").val(),
+          subject: jQuery('.form-' + index).find("input[name=subject]").val()
+        };
+        ajaxSend('.form-' + index, t);
+      }
+    });
+
+  });
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: "POST",
+      url: "sendmail.php",
+      data: data,
+      success: function() {
+        $(".modal").popup("hide");
+        $("#thanks").popup("show");
+        setTimeout(function() {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
+
+//-------------------------скорость якоря---------------------------------------
+  $(".header__btn").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top - 60}, 'slow', 'swing');
+  });
+
+  
+});
